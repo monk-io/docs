3f8461a0ba57412a542a54daa2fcbdefd3405359
Alexander <alexander.vvoo@gmail.com>
2021-07-19 18:50:00 +0300

    Features/main page tabs updates (#32)
    * was added icons in the main nav items
    
    * was added icons in the main nav items

--- src/components/bigNavItem/index.js
+++ src/components/bigNavItem/index.js
@@ -3,10 +3,11 @@ import Link from '@docusaurus/Link';
 
 import './index.scss';
 
-export default ({to, title, desc}) => {
+export default ({to, title, desc, icon}) => {
   return (
     <Link to={to} className="col col--4 big-nav-item">
       <div className="big-nav-item__content">
+        {icon && <img src={icon} className="big-nav-item__icon" />}
         <span className="big-nav-item__title">{title}</span>
         <p className="big-nav-item__desc">{desc}</p>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,31 +10,37 @@ export default function Home() {
             to: "/docs",
             title: "About Monk",
             desc: "Learn about Monk and its features.",
+            icon: "/img/mainNavItems/about-monk.svg"
         },
         {
             to: "/docs/monk-in-10",
             title: "Monk Basics",
             desc: "From zero to cluster in 10 min.",
+            icon: "/img/mainNavItems/monk-basics.svg"
         },
         {
             to: "/docs/get-monk",
             title: "Install Monk",
             desc: "Download, install and update Monk.",
+            icon: "/img/mainNavItems/install-monk.svg"
         },
         {
             to: "/docs/lifecycle/cluster-overview",
             title: "Guides",
             desc: "Learn Monk hands on.",
+            icon: "/img/mainNavItems/guides.svg"
         },
         {
             to: "/docs/support",
             title: "Support & Contact",
             desc: "Join the community, get in touch.",
+            icon: "/img/mainNavItems/support-contact.svg"
         },
         {
             to: "/docs/monkscript",
             title: "MonkScript Reference",
             desc: "Monk templating language in detail.",
+            icon: "/img/mainNavItems/monkscript.svg"
         },
     ];
     return (
