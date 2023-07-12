27cc0b6d3dda41b5e22666b2d490756700ac110a
Marcin Gasperowicz <xnooga@gmail.com>
2023-07-12 13:07:14 +0100

    Add chatwoot to docs

--- static/js/chatwoot.js
+++ static/js/chatwoot.js
@@ -1,18 +1,14 @@
 (function(d,t) {
-  let g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-  const BASE_URL="https://chat.monk.io";
-  const runChatwootSDK = (token = "Kmbzacatn443pvGdxgY3Jaz5") => window.chatwootSDK.run({
-    websiteToken: token,
-    baseUrl: BASE_URL
-  });
-
+  var BASE_URL="https://help.monk.io";
+  var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
   g.src=BASE_URL+"/packs/js/sdk.js";
+  g.defer = true;
+  g.async = true;
   s.parentNode.insertBefore(g,s);
-
   g.onload=function(){
-    fetch('https://chat.monk.io/monktoken/home')
-    .then(response => response.json())
-    .then((r) => runChatwootSDK(r.token))
-    .catch(e => runChatwootSDK())
+    window.chatwootSDK.run({
+      websiteToken: 'tT8eHipK7hc1wmSem92ztdyw',
+      baseUrl: BASE_URL
+    })
   }
 })(document,"script");
